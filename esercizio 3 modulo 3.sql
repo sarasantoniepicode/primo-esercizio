c1731c2dea59a7b3c321cdd32483d56e2b6d787e
sarasantoniepicode <135055166+sarasantoniepicode@users.noreply.github.com>
2023-08-02 20:44:41 +0200

    esercizio e modulo 3

---
+++ esercizio 3 modulo 3.sql
@@ -0,0 +1,47 @@
+create table Store (
+IDstore int, 
+indirizzofisico text not null,
+numeroditelefono text not null,
+primary key (IDstore)
+);
+
+create table Dipendente (
+codicefiscale varchar(16),
+nome text not null,
+titolodistudio text not null,
+recapito text not null,
+ruolo text not null,
+IDstore int not null,
+datainizioimpiego date not null,
+datafineimpiego date,
+primary key (codicefiscale,datainizioimpiego),
+foreign key (IDstore) references Store(IDstore) on update cascade on delete no action);
+
+create table Settore (
+IDstore int,
+IDsettore int,
+primary key(IDstore,IDsettore),
+foreign key (IDstore) references Store(IDstore) on update cascade on delete no action);
+
+create table Videogioco (
+titolo varchar(100),
+sviluppatore varchar(100),
+annodiuscita year not null,
+prezzo decimal(5,2) not null,
+genere text not null,
+remake boolean not null,
+primary key (titolo,sviluppatore)
+);
+
+create table Sitrova (
+IDstore int,
+IDsettore int,
+posizione text not null,
+disponibilità boolean not null,
+titolo varchar(100),
+sviluppatore varchar(100) not null,
+primary key (IDstore,IDsettore,settore,titolo,sviluppatore),
+foreign key (IDstore, IDsettore) references Store(IDstore,IDsettore) on update cascade on delete no action,
+foreign key (titolo,sviluppatore) references Videogioco(titolo,sviluppatore) on update cascade on delete no action
+);
+
