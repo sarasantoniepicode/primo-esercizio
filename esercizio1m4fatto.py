7e1d7006c9e45a78574fa417e50a1b589bc538e3
sarasantoniepicode <135055166+sarasantoniepicode@users.noreply.github.com>
2023-09-05 20:40:39 +0200

    esercizio1m4

---
+++ esercizio1m4fatto.py
@@ -0,0 +1,18 @@
+studenti=25
+
+studenti=25
+print(studenti)
+
+studenti=25
+altri_studenti=3
+
+studenti=25
+altri_studenti=3
+somma_studenti=studenti+altri_studenti
+print(somma_studenti)
+
+lista_numeri=[0,1,2,3,4,5]
+print("la lista è questa: ", lista_numeri)
+
+lista_film=["il signore degli anelli","philadelfia","romeo e giulietta"]
+print(lista_film)
